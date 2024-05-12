da9747c7faef4479baeebaede6afd57c92d482d1
MhaceMojica <mhacegwyneth04@gmail.com>
2024-05-12 22:12:57 +0800

    Mojica, Mhace Gwyneth A.

--- src/bankmanagementsystem/AccInterfaceUI.java
+++ src/bankmanagementsystem/AccInterfaceUI.java
@@ -1,56 +1,60 @@
 package bankmanagementsystem;
 
-
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
 import javax.swing.*;
+import java.awt.*;
 
 
-public class AccInterfaceUI {
+public class AccInterfaceUI extends JFrame  {
     
     private JFrame Acc;
-    private ImageIcon BgColor;
-    private JLabel label1;
+    private ImageIcon BgColor, iconImage;
     private JPanel panel1;
+    private JLabel label1, Depo;
     
-    
-    
-    AccInterfaceUI(){
-    //frame
-    Acc = new JFrame();
-    Acc.setTitle("BANK MANAGEMENT SYSTEM");
-    Acc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    Acc.setResizable(false);
-    Acc.setSize(700, 500);
+    public AccInterfaceUI() {
+        //frame
+        Acc = new JFrame();
+        Acc.setTitle("BANK MANAGEMENT SYSTEM");
+        Acc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Acc.setResizable(false);
+        Acc.setSize(700, 500);
         
-    // logo
-    BgColor = new ImageIcon("C:\\Users\\mhace\\OneDrive\\Desktop\\OOP\\12.jpg");
-    Acc.setIconImage(BgColor.getImage());
-    Acc.setLayout(new BorderLayout());
-    Acc.getContentPane().setBackground(new Color(169, 217, 186));
-
-            
-    //label "bank of bsit"
-    label1 = new JLabel("BOB - BANK OF BSIT");
-    label1.setForeground(Color.BLACK);
-    label1.setFont(new Font("Courier New", Font.BOLD, 30));
-    label1.setHorizontalAlignment(JLabel.CENTER);
-    label1.setBorder(BorderFactory.createEmptyBorder(20, 0, 20, 0));
-    Acc.add(label1, BorderLayout.NORTH);
-   
-
-    // panel at the bg of the icons
-    panel1 = new JPanel();
-    panel1.setBackground(new Color(233, 227, 200));
-    panel1.setPreferredSize(new Dimension(10, 50)); 
-    panel1.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50));
-    Acc.add(panel1, BorderLayout.CENTER); 
+        // logo
+        BgColor = new ImageIcon("C:\\Users\\mhace\\OneDrive\\Desktop\\OOP\\12.jpg");
+        Acc.setIconImage(BgColor.getImage());
+        Acc.setLayout(new BorderLayout());
+        Acc.getContentPane().setBackground(new Color(169, 217, 186));
     
+        //label "bank of bsit"
+        ImageIcon iconImage = new ImageIcon("C:\\Users\\mhace\\OneDrive\\Desktop\\OOP\\logo.png");
+        Image sizeImage = iconImage.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+        ImageIcon image = new ImageIcon(sizeImage);
+        label1 = new JLabel("BOB - BANK OF BSIT");
+        label1.setForeground(Color.BLACK);
+        label1.setFont(new Font("Courier New", Font.BOLD, 35));
+        label1.setHorizontalAlignment(JLabel.CENTER);
+        label1.setBorder(BorderFactory.createEmptyBorder(20, 0, 20, 0));
+        label1.setIcon(image);
+        Acc.add(label1, BorderLayout.NORTH);
     
-    Acc.setVisible(true);
-    
- 
+        // panel at the bg of the icons
+        panel1 = new JPanel();
+        panel1.setBackground(new Color(233, 227, 200));
+        panel1.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50));
+        Acc.add(panel1, BorderLayout.CENTER);
+        
+        //Deposit Click!!
+        Depo = new JLabel("Make A Deposit");
+        Depo.setBounds(200, 200, 100, 100);
+        Depo.setPreferredSize(new Dimension(100, 50)); 
+        Depo.setBackground(Color.blue);
+        Depo.setOpaque(true);
+        Depo.addMouseListener(new Deposit());
+        panel1.add(Depo);
+        
+        
+        Acc.setVisible(true);
     }
+
+
 }
