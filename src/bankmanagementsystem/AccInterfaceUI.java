0abbed3c8c24bb8325e77676b6607aee41b0c5b9
MhaceMojica <mhacegwyneth04@gmail.com>
2024-05-15 00:29:21 +0800

    Mojica, Mhace Gwyneth A.

--- src/bankmanagementsystem/AccInterfaceUI.java
+++ src/bankmanagementsystem/AccInterfaceUI.java
@@ -7,10 +7,11 @@ import java.awt.*;
 public class AccInterfaceUI extends JFrame  {
     
     private JFrame Acc;
-    private ImageIcon BgColor, iconImage, Depic;
-    private JPanel depanel, panel2;
-    private JLabel label1, Depo;
-
+    private ImageIcon BgColor, iconImage, Depic, depImage;
+    private JPanel panel1, depanel, deposit;
+    private JLabel label1, lblInfo, AccNamelbl, AccNumlbl, balancelbl;
+    private JTextField tfBlank, AccNametf, AccNumtf, balancetf;
+    private JButton depoBtn, WdrawBtn, transBtn, pinBtn, ExitBtn;
     
     public AccInterfaceUI() {
         //frame
@@ -18,62 +19,112 @@ public class AccInterfaceUI extends JFrame  {
         Acc.setTitle("BANK MANAGEMENT SYSTEM");
         Acc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Acc.setResizable(false);
-        Acc.setSize(700, 500);
+        Acc.setSize(600, 480);
         
         // logo
         BgColor = new ImageIcon("C:\\Users\\mhace\\OneDrive\\Desktop\\OOP\\12.jpg");
         Acc.setIconImage(BgColor.getImage());
-        Acc.getContentPane().setBackground(new Color(169, 217, 186));
+        Acc.getContentPane().setBackground(new Color(100,50,78));
         Acc.setLayout(new BorderLayout());
         
         //label "bank of bsit"
-        ImageIcon iconImage = new ImageIcon("C:\\Users\\mhace\\OneDrive\\Desktop\\OOP\\logo.png");
+        ImageIcon iconImage = new ImageIcon("logo1.png");
         Image sizeImage = iconImage.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
         ImageIcon image = new ImageIcon(sizeImage);
         label1 = new JLabel("BOB - BANK OF BSIT");
-        label1.setForeground(Color.BLACK);
+        label1.setForeground(Color.WHITE);
         label1.setFont(new Font("Courier New", Font.BOLD, 35));
         label1.setHorizontalAlignment(JLabel.CENTER);
-        label1.setBorder(BorderFactory.createEmptyBorder(20, 0, 20, 0));
         label1.setIcon(image);
         Acc.add(label1, BorderLayout.NORTH);
-      
-      
-        //buttons
-        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        buttonPanel.setBackground(new Color(233, 227, 200));
-        buttonPanel.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50));
-        Acc.add(buttonPanel, BorderLayout.CENTER);
-
-//Withdraw
-        JButton withdrawButton = new JButton();
-        ImageIcon withdrawIcon = new ImageIcon("C:\\Users\\Sarvin\\Downloads\\withdraw-money-icon-free-vector-removebg-preview.png");
-        Image withdrawScaledImage = withdrawIcon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
-        withdrawButton.setIcon(new ImageIcon(withdrawScaledImage));
-        buttonPanel.add(withdrawButton); // Add "Withdraw" button to the panel
-
-        //Deposit
-        JButton depoButton = new JButton();
-        ImageIcon depositIcon = new ImageIcon("C:\\Users\\Sarvin\\Downloads\\images-removebg-preview (1).png");
-        Image depositScaledImage = depositIcon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
-        depoButton.setIcon(new ImageIcon(depositScaledImage));
-        buttonPanel.add(depoButton); // Add "Deposit" button to the panel
-
-        //Transactions
-        JButton transButton = new JButton();
-        ImageIcon transIcon = new ImageIcon("C:\\Users\\Sarvin\\Downloads\\kisspng-checks-bank-account-clip-art-transaction-account-5cf31ba193e545.0629072015594361936058-removebg-preview (1).png");
-        Image transScaledImage = transIcon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
-        transButton.setIcon(new ImageIcon(transScaledImage));
-        buttonPanel.add(transButton);
-
-        //Pin
-        JButton pinButton = new JButton();
-        ImageIcon pinIcon = new ImageIcon("C:\\Users\\Sarvin\\Downloads\\istockphoto-927672636-612x612-removebg-preview.png");
-        Image pinScaledImage = pinIcon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
-        pinButton.setIcon(new ImageIcon(pinScaledImage));
-        buttonPanel.add(pinButton);
-
         
+        //panel at the bg of the icons
+        panel1 = new JPanel();
+        panel1.setBackground(new Color(100,50,78));
+        panel1.setBounds(50, 50, 500, 350);
+        Acc.add(panel1, BorderLayout.CENTER); 
+        
+        depanel = new JPanel();
+        depanel.setLayout(null);  
+        depanel.setBackground(new Color(220, 190, 200));
+        depanel.setPreferredSize(new Dimension(550, 300));
+        panel1.add(depanel, BorderLayout.CENTER);
+        
+        // textfields
+        lblInfo = new JLabel("ACCOUNT INFORMATION");
+        lblInfo.setBounds(170,0,300,50);
+        Font font = new Font("Aptos", Font.BOLD, 20);
+        lblInfo.setFont(font);
+        
+        //AccInfo
+        tfBlank = new JTextField();
+        tfBlank.setBounds(0,0,0,0);
+        
+        AccNamelbl = new JLabel ("Account Name:");
+        AccNamelbl.setBounds (40, 50, 100, 40);
+        AccNametf = new JTextField();
+        AccNametf.setBounds(170, 60, 150, 20);
+        AccNametf.setHorizontalAlignment(JTextField.CENTER);
+        
+        AccNumlbl = new JLabel ("Account Number:");
+        AccNumlbl.setBounds (40, 80, 140, 40);
+        AccNumtf = new JTextField();
+        AccNumtf.setBounds(170, 90, 150, 20);
+        AccNumtf.setHorizontalAlignment(JTextField.CENTER);
+        
+        //Balance
+        balancelbl = new JLabel ("Balance");
+        balancelbl.setBounds (420, 50, 100, 30);
+        balancetf = new JTextField();
+        balancetf.setBounds(370, 82, 150, 25);
+        balancetf.setHorizontalAlignment(JTextField.CENTER);
+
+        //Deposit btn
+        depoBtn = new JButton();
+        depImage = new ImageIcon("dep.png");
+        Image depsize = depImage.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+        depoBtn.setIcon(new ImageIcon(depsize));
+        depoBtn.setBounds(25, 150, 115, 120);  
+        
+        //Withdraw
+        WdrawBtn = new JButton();
+        ImageIcon WdraImage = new ImageIcon("Wdraw.png");
+        Image Wdrawsize = WdraImage.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+        WdrawBtn.setIcon(new ImageIcon(Wdrawsize));
+        WdrawBtn.setBounds(150, 150, 115, 120);
+        
+        //Transactions
+        transBtn = new JButton();
+        ImageIcon transIcon = new ImageIcon("Transfer.png");
+        Image transScaledImage = transIcon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+        transBtn .setIcon(new ImageIcon(transScaledImage));
+        transBtn.setBounds(275, 150, 115, 120);
+        
+        //Change Pin
+        pinBtn = new JButton();
+        ImageIcon pinIcon = new ImageIcon("Pin.png");
+        Image pinScaledImage = pinIcon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+        pinBtn.setIcon(new ImageIcon(pinScaledImage));
+        pinBtn.setBounds(400, 150, 115, 120);
+        
+        //Exit
+        ExitBtn = new JButton("EXIT");
+        ExitBtn.setBounds(400, 275, 100, 20);
+        ExitBtn.setFont(new Font("Arial", Font.BOLD, 10));
+
+        depanel.add(lblInfo);
+        depanel.add(AccNamelbl);
+        depanel.add(AccNumlbl);
+        depanel.add(AccNametf);
+        depanel.add(AccNumtf);
+        depanel.add(balancelbl);
+        depanel.add(balancetf);
+        depanel.add(depoBtn);
+        depanel.add(WdrawBtn);
+        depanel.add(transBtn );
+        depanel.add(pinBtn);
+        panel1.add(ExitBtn);
+
         Acc.setVisible(true);
     }
 
