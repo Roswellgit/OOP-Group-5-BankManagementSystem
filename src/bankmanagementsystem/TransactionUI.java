773de9dd975847bb933eff0b81ef5cc22a73d4b3
selldadada <crisellesayo6@gmail.com>
2024-07-03 22:42:35 +0800

    Criselle Sayo

--- src/bankmanagementsystem/TransactionUI.java
+++ src/bankmanagementsystem/TransactionUI.java
@@ -2,21 +2,25 @@ package bankmanagementsystem;
 
 import javax.swing.*;
 import java.awt.*;
+import static java.awt.Color.black;
 import java.awt.event.*;
 import javax.swing.table.DefaultTableModel;
 import java.sql.*;
+import javax.swing.border.LineBorder;
+import javax.swing.table.*;
 
 public class TransactionUI extends JFrame implements ActionListener {
 
     private JFrame Acc;
-    private ImageIcon BgColor, iconImage;
+    private ImageIcon BgColor;
     private JPanel panel1, transpanel;
-    private JLabel label1, lblInfo,  balancelbl;
-    private JTextField tfBlank, depotf;
+    private JLabel label1;
     private JButton  ExitBtn;
     private Icon image;
-    private JTable transTable;
-    private final JScrollPane transtable;
+    private JTable tblRecords;
+    private JScrollPane spScroll;
+    private DefaultTableModel ColumnHeaders;
+    private JTableHeader TableHeader;
 
     public TransactionUI() {
 
@@ -33,7 +37,7 @@ public class TransactionUI extends JFrame implements ActionListener {
         Acc.setIconImage(BgColor.getImage());
         Acc.getContentPane().setBackground(new Color(100, 50, 78));
         Acc.setLayout(new BorderLayout());
-        label1 = new JLabel("Bank Transaction");
+        label1 = new JLabel(" Bank Transactions");
         label1.setForeground(Color.WHITE);
         label1.setFont(new Font("ARIAL BLACK", Font.BOLD, 20));
         label1.setHorizontalAlignment(JLabel.LEFT);
@@ -49,86 +53,22 @@ public class TransactionUI extends JFrame implements ActionListener {
         transpanel.setLayout(null);
         transpanel.setBackground(new Color(220, 190, 200));
         transpanel.setPreferredSize(new Dimension(520, 380));
-        panel1.add(transpanel, BorderLayout.CENTER);
-
-        // Account Name 
-       
+        panel1.add(transpanel, BorderLayout.CENTER);      
         
-       
-        // Account Number
-     
+        //table & scroll
+        ColumnHeaders = new DefaultTableModel(new Object[] {"Transaction ID", "Date", "Type", "Amount", "Balance"}, 0);
+        tblRecords = new JTable(ColumnHeaders);
+        tblRecords.setOpaque(false);
         
+        TableHeader = tblRecords.getTableHeader();
+        tblRecords.getTableHeader().setBackground(new Color(181,103,110));
+        tblRecords.getTableHeader().setForeground(black);
+        tblRecords.getTableHeader().setFont(new Font("Aptos", Font.PLAIN, 12));
         
-
-        // Account Balance 
-        balancelbl = new JLabel("Account Balance: ₱ ");
-        balancelbl.setBounds(265, 40, 150, 25);
-        
-        
-//
-//        // Table
-//        String[] columnNames = {"Transaction ID", "Date", "Type", "Amount", "Balance"};
-//        Object[][] data = {
-//            {"1", "", "", "", ""},
-//            {"2", "", "", "", ""},
-//            {"3", "", "", "", ""},
-//            {"4", "", "", "", ""},
-//            {"5", "", "", "", ""}
-//        };
-//        transTable = new JTable(new DefaultTableModel(data, columnNames));
-//        transtable = new JScrollPane(transTable);
-//        transtable.setBounds(30, 120, 450, 200);
-//        transpanel.add(transtable);
-// transTable = new JTable();
-//        transtable = new JScrollPane(transTable);
-//        transTable.setBounds(30, 120, 450, 200); 
-//        transpanel.add(transTable); 
-//        transpanel.add(transtable);
-//        transTable.setModel(new javax.swing.table.DefaultTableModel(
-//        new Object[][]{
-//            
-//        },
-//        new String[]{"Transaction ID", "Date", "Type", "Amount", "Balance"}
-//        ));
-//        try{
-//    Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_bank","root","root");
-//    
-//    Statement st = con.createStatement();
-//    
-//    String q =  "SELECT * FROM history";
-//    ResultSet rs = st.executeQuery(q);
-//    
-//    while(rs.next()){
-//        String accnum = rs.getString("acc_num");
-//        String date = rs.getString("date");
-//        String type = rs.getString("type");
-//        String amnt = rs.getString("amnt");
-//        String accbal= rs.getString("acc_bal");
-//        
-//        
-//        String tbData[] = {accnum,date,type,amnt,accbal};
-//        DefaultTableModel record = (DefaultTableModel)transTable.getModel();
-//        
-//        record.addColumn("Account Number");
-//        record.addRow(tbData);
-//      
-//        
-//    }
-//    con.close();
-//}
-//catch(Exception ex){
-//    System.out.println(ex);
-//}
-        transTable = new JTable();
-        transtable = new JScrollPane(transTable);
-        transTable.setBounds(30, 120, 450, 200);
-        transpanel.add(transTable);
-        transpanel.add(transtable);
-        transTable.setModel(new javax.swing.table.DefaultTableModel(
-        
-        new Object[][]{},
-        new String[]{"Transaction ID", "Date", "Type", "Amount", "Balance"}
-        ));
+        spScroll = new JScrollPane(tblRecords);
+        spScroll.setBounds(30, 40, 450, 300);
+        spScroll.setBorder(new LineBorder(Color.BLACK, 1));
+        transpanel.add(spScroll);
 
         try {
  
@@ -138,7 +78,7 @@ public class TransactionUI extends JFrame implements ActionListener {
         String q = "SELECT * FROM history";
         ResultSet rs = st.executeQuery(q);
 
-        DefaultTableModel model = (DefaultTableModel) transTable.getModel();
+        DefaultTableModel model = (DefaultTableModel) tblRecords.getModel();
 
         while (rs.next()) {
             String accnum = rs.getString("acc_num");
@@ -160,12 +100,7 @@ catch(Exception ex){
         ExitBtn.setBounds(400, 275, 100, 20);
         ExitBtn.setFont(new Font("Arial", Font.BOLD, 10));
         ExitBtn.addActionListener(this);
-        panel1.add(ExitBtn);
-        
-        
-        
-        transpanel.add(balancelbl);
-        
+        panel1.add(ExitBtn);       
         
         Acc.setVisible(true);
     }
